6fe164a915425efcfe6d2a89e7d066558a7b8da1
Gus The IT Guru <gus@gustheitguru.com>
2020-09-02 11:11:39 -0500

    Send Popup message to call computers
    Send a Popup message about maintance windows to all computers on the domain

---
+++ Send popup message to all comps.ps1
@@ -0,0 +1,9 @@
+#enter Message
+$Message = Read-Host Enter Message
+
+#Send Messgae to computers on Domain and message will last 15 minutes on the screen. 
+#You can remove the /TIME: to leave message on screen for ever
+
+(Get-ADComputer -SearchBase "OU=Computers,OU=MyBusiness,DC=gbchdomain,DC=local" -Filter *).Name | Foreach-Object {
+	msg * /server:$_ /TIME:900 "$Message"
+}
